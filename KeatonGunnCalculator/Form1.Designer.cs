e8eeec1d6d5899519fd87c465cef8a45246bd066
Keaton Gunn <keatongunn@hotmail.com>
2022-04-13 17:05:00 -0500

    Add project files.

---
+++ KeatonGunnCalculator/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace KeatonGunnCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumBtn1 = new System.Windows.Forms.Button();
+            this.NumBtn2 = new System.Windows.Forms.Button();
+            this.NumBtn3 = new System.Windows.Forms.Button();
+            this.NumBtn4 = new System.Windows.Forms.Button();
+            this.NumBtn5 = new System.Windows.Forms.Button();
+            this.NumBtn6 = new System.Windows.Forms.Button();
+            this.NumBtn7 = new System.Windows.Forms.Button();
+            this.NumBtn8 = new System.Windows.Forms.Button();
+            this.NumBtn9 = new System.Windows.Forms.Button();
+            this.NumDisplay = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.DecBtn = new System.Windows.Forms.Button();
+            this.NumBtn0 = new System.Windows.Forms.Button();
+            this.EqualButton = new System.Windows.Forms.Button();
+            this.MinBtn = new System.Windows.Forms.Button();
+            this.PlusBtn = new System.Windows.Forms.Button();
+            this.XBtn = new System.Windows.Forms.Button();
+            this.DivBtn = new System.Windows.Forms.Button();
+            this.BinBtn = new System.Windows.Forms.Button();
+            this.DecConvBtn = new System.Windows.Forms.Button();
+            this.DelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NumBtn1
+            // 
+            this.NumBtn1.AccessibleDescription = "Click this button to input number 1";
+            this.NumBtn1.AccessibleName = "NumberButton1";
+            this.NumBtn1.Location = new System.Drawing.Point(315, 156);
+            this.NumBtn1.Name = "NumBtn1";
+            this.NumBtn1.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn1.TabIndex = 0;
+            this.NumBtn1.Text = "1";
+            this.NumBtn1.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn2
+            // 
+            this.NumBtn2.AccessibleDescription = "Click this button to input number 2";
+            this.NumBtn2.AccessibleName = "NumberButton2";
+            this.NumBtn2.Location = new System.Drawing.Point(396, 156);
+            this.NumBtn2.Name = "NumBtn2";
+            this.NumBtn2.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn2.TabIndex = 1;
+            this.NumBtn2.Text = "2";
+            this.NumBtn2.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn3
+            // 
+            this.NumBtn3.AccessibleDescription = "Click this button to input number 3";
+            this.NumBtn3.AccessibleName = "NumberButton3";
+            this.NumBtn3.Location = new System.Drawing.Point(477, 156);
+            this.NumBtn3.Name = "NumBtn3";
+            this.NumBtn3.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn3.TabIndex = 2;
+            this.NumBtn3.Text = "3";
+            this.NumBtn3.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn4
+            // 
+            this.NumBtn4.AccessibleDescription = "Click this button to input number 4";
+            this.NumBtn4.AccessibleName = "NumberButton4";
+            this.NumBtn4.Location = new System.Drawing.Point(315, 206);
+            this.NumBtn4.Name = "NumBtn4";
+            this.NumBtn4.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn4.TabIndex = 3;
+            this.NumBtn4.Text = "4";
+            this.NumBtn4.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn5
+            // 
+            this.NumBtn5.AccessibleDescription = "Click this button to input number 5";
+            this.NumBtn5.AccessibleName = "NumberButton5";
+            this.NumBtn5.Location = new System.Drawing.Point(396, 206);
+            this.NumBtn5.Name = "NumBtn5";
+            this.NumBtn5.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn5.TabIndex = 4;
+            this.NumBtn5.Text = "5";
+            this.NumBtn5.UseVisualStyleBackColor = true;
+            this.NumBtn5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // NumBtn6
+            // 
+            this.NumBtn6.AccessibleDescription = "Click this button to input number 6";
+            this.NumBtn6.AccessibleName = "NumberButton6";
+            this.NumBtn6.Location = new System.Drawing.Point(477, 206);
+            this.NumBtn6.Name = "NumBtn6";
+            this.NumBtn6.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn6.TabIndex = 5;
+            this.NumBtn6.Text = "6";
+            this.NumBtn6.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn7
+            // 
+            this.NumBtn7.AccessibleDescription = "Click this button to input number 7";
+            this.NumBtn7.AccessibleName = "NumberButton7";
+            this.NumBtn7.Location = new System.Drawing.Point(315, 252);
+            this.NumBtn7.Name = "NumBtn7";
+            this.NumBtn7.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn7.TabIndex = 6;
+            this.NumBtn7.Text = "7";
+            this.NumBtn7.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn8
+            // 
+            this.NumBtn8.AccessibleDescription = "Click this button to input number 8";
+            this.NumBtn8.AccessibleName = "NumberButton8";
+            this.NumBtn8.Location = new System.Drawing.Point(396, 252);
+            this.NumBtn8.Name = "NumBtn8";
+            this.NumBtn8.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn8.TabIndex = 7;
+            this.NumBtn8.Text = "8";
+            this.NumBtn8.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn9
+            // 
+            this.NumBtn9.AccessibleDescription = "Click this button to input number 9";
+            this.NumBtn9.AccessibleName = "NumberButton9";
+            this.NumBtn9.Location = new System.Drawing.Point(477, 252);
+            this.NumBtn9.Name = "NumBtn9";
+            this.NumBtn9.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn9.TabIndex = 8;
+            this.NumBtn9.Text = "9";
+            this.NumBtn9.UseVisualStyleBackColor = true;
+            // 
+            // NumDisplay
+            // 
+            this.NumDisplay.AccessibleDescription = "The screen that holds the current number display";
+            this.NumDisplay.AccessibleName = "NumberDisplay";
+            this.NumDisplay.Location = new System.Drawing.Point(315, 55);
+            this.NumDisplay.Multiline = true;
+            this.NumDisplay.Name = "NumDisplay";
+            this.NumDisplay.Size = new System.Drawing.Size(318, 49);
+            this.NumDisplay.TabIndex = 9;
+            this.NumDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Clear
+            // 
+            this.Clear.AccessibleDescription = "Clears everything currently stored in the calculator";
+            this.Clear.AccessibleName = "ClearButton";
+            this.Clear.Location = new System.Drawing.Point(315, 110);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 40);
+            this.Clear.TabIndex = 10;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            // 
+            // DecBtn
+            // 
+            this.DecBtn.AccessibleDescription = "click this button to add a decimal to the number display";
+            this.DecBtn.AccessibleName = "DecimalButton";
+            this.DecBtn.Location = new System.Drawing.Point(315, 298);
+            this.DecBtn.Name = "DecBtn";
+            this.DecBtn.Size = new System.Drawing.Size(75, 40);
+            this.DecBtn.TabIndex = 11;
+            this.DecBtn.Text = ".";
+            this.DecBtn.UseVisualStyleBackColor = true;
+            // 
+            // NumBtn0
+            // 
+            this.NumBtn0.AccessibleDescription = "Click this button to enter number 0 ";
+            this.NumBtn0.AccessibleName = "NumberButton0";
+            this.NumBtn0.Location = new System.Drawing.Point(396, 298);
+            this.NumBtn0.Name = "NumBtn0";
+            this.NumBtn0.Size = new System.Drawing.Size(75, 40);
+            this.NumBtn0.TabIndex = 12;
+            this.NumBtn0.Text = "0";
+            this.NumBtn0.UseVisualStyleBackColor = true;
+            this.NumBtn0.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // EqualButton
+            // 
+            this.EqualButton.AccessibleDescription = "Click this button to solve the stored operations";
+            this.EqualButton.AccessibleName = "EqualButton";
+            this.EqualButton.Location = new System.Drawing.Point(477, 298);
+            this.EqualButton.Name = "EqualButton";
+            this.EqualButton.Size = new System.Drawing.Size(75, 40);
+            this.EqualButton.TabIndex = 13;
+            this.EqualButton.Text = "=";
+            this.EqualButton.UseVisualStyleBackColor = true;
+            // 
+            // MinBtn
+            // 
+            this.MinBtn.AccessibleDescription = "Click this button to add a subtraction equation into the number display";
+            this.MinBtn.AccessibleName = "MinusButton";
+            this.MinBtn.Location = new System.Drawing.Point(558, 298);
+            this.MinBtn.Name = "MinBtn";
+            this.MinBtn.Size = new System.Drawing.Size(75, 40);
+            this.MinBtn.TabIndex = 14;
+            this.MinBtn.Text = "-";
+            this.MinBtn.UseVisualStyleBackColor = true;
+            // 
+            // PlusBtn
+            // 
+            this.PlusBtn.AccessibleDescription = "Click this button to enter a plus operation into the number display";
+            this.PlusBtn.AccessibleName = "PlusButton";
+            this.PlusBtn.Location = new System.Drawing.Point(558, 252);
+            this.PlusBtn.Name = "PlusBtn";
+            this.PlusBtn.Size = new System.Drawing.Size(75, 40);
+            this.PlusBtn.TabIndex = 15;
+            this.PlusBtn.Text = "+";
+            this.PlusBtn.UseVisualStyleBackColor = true;
+            // 
+            // XBtn
+            // 
+            this.XBtn.AccessibleDescription = "Click this button to input a multiplication input into the number display";
+            this.XBtn.AccessibleName = "MultiplicationButton";
+            this.XBtn.Location = new System.Drawing.Point(558, 206);
+            this.XBtn.Name = "XBtn";
+            this.XBtn.Size = new System.Drawing.Size(75, 40);
+            this.XBtn.TabIndex = 16;
+            this.XBtn.Text = "X";
+            this.XBtn.UseVisualStyleBackColor = true;
+            // 
+            // DivBtn
+            // 
+            this.DivBtn.AccessibleDescription = "Click this button to add a divide operation into the number display";
+            this.DivBtn.AccessibleName = "DivideButton";
+            this.DivBtn.Location = new System.Drawing.Point(558, 156);
+            this.DivBtn.Name = "DivBtn";
+            this.DivBtn.Size = new System.Drawing.Size(75, 40);
+            this.DivBtn.TabIndex = 17;
+            this.DivBtn.Text = "/";
+            this.DivBtn.UseVisualStyleBackColor = true;
+            // 
+            // BinBtn
+            // 
+            this.BinBtn.AccessibleDescription = "Click this button to convert a positive integer to a binary number";
+            this.BinBtn.AccessibleName = "BinaryButton";
+            this.BinBtn.Location = new System.Drawing.Point(396, 110);
+            this.BinBtn.Name = "BinBtn";
+            this.BinBtn.Size = new System.Drawing.Size(75, 40);
+            this.BinBtn.TabIndex = 18;
+            this.BinBtn.Text = "BIN";
+            this.BinBtn.UseVisualStyleBackColor = true;
+            // 
+            // DecConvBtn
+            // 
+            this.DecConvBtn.AccessibleDescription = "Click this button to convert a binary number to a decimal number";
+            this.DecConvBtn.AccessibleName = "DecimalConvertButton";
+            this.DecConvBtn.Location = new System.Drawing.Point(477, 110);
+            this.DecConvBtn.Name = "DecConvBtn";
+            this.DecConvBtn.Size = new System.Drawing.Size(75, 40);
+            this.DecConvBtn.TabIndex = 19;
+            this.DecConvBtn.Text = "DEC";
+            this.DecConvBtn.UseVisualStyleBackColor = true;
+            // 
+            // DelBtn
+            // 
+            this.DelBtn.AccessibleDescription = "Click this button to delete the most recent number input in the number display";
+            this.DelBtn.AccessibleName = "DeleteButton";
+            this.DelBtn.Location = new System.Drawing.Point(558, 110);
+            this.DelBtn.Name = "DelBtn";
+            this.DelBtn.Size = new System.Drawing.Size(75, 40);
+            this.DelBtn.TabIndex = 20;
+            this.DelBtn.Text = "DEL";
+            this.DelBtn.UseVisualStyleBackColor = true;
+            this.DelBtn.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 450);
+            this.Controls.Add(this.DelBtn);
+            this.Controls.Add(this.DecConvBtn);
+            this.Controls.Add(this.BinBtn);
+            this.Controls.Add(this.DivBtn);
+            this.Controls.Add(this.XBtn);
+            this.Controls.Add(this.PlusBtn);
+            this.Controls.Add(this.MinBtn);
+            this.Controls.Add(this.EqualButton);
+            this.Controls.Add(this.NumBtn0);
+            this.Controls.Add(this.DecBtn);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.NumDisplay);
+            this.Controls.Add(this.NumBtn9);
+            this.Controls.Add(this.NumBtn8);
+            this.Controls.Add(this.NumBtn7);
+            this.Controls.Add(this.NumBtn6);
+            this.Controls.Add(this.NumBtn5);
+            this.Controls.Add(this.NumBtn4);
+            this.Controls.Add(this.NumBtn3);
+            this.Controls.Add(this.NumBtn2);
+            this.Controls.Add(this.NumBtn1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button NumBtn1;
+        private Button NumBtn2;
+        private Button NumBtn3;
+        private Button NumBtn4;
+        private Button NumBtn5;
+        private Button NumBtn6;
+        private Button NumBtn7;
+        private Button NumBtn8;
+        private Button NumBtn9;
+        private TextBox NumDisplay;
+        private Button Clear;
+        private Button DecBtn;
+        private Button NumBtn0;
+        private Button EqualButton;
+        private Button MinBtn;
+        private Button PlusBtn;
+        private Button XBtn;
+        private Button DivBtn;
+        private Button BinBtn;
+        private Button DecConvBtn;
+        private Button DelBtn;
+    }
+}
---
+++ KeatonGunnCalculator/Form1.cs
@@ -0,0 +1,30 @@
+namespace KeatonGunnCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
